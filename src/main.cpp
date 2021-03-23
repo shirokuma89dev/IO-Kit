b237f3b3a555c457aaae4f92e89a079250555aac
Shirokuma@胡瓜 <shirokuma89.git@icloud.com>
2021-03-23 21:00:03 +0900

    動作確認用プログラムをmain.cppに追記

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,10 @@
 
 #include "include/IO-Kit.h"
 
+Output LED = 13;
+
 void setup() {
-  // put your setup code here, to run once:
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 }
