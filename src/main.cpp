04c2c39942e490ce50a972acaf63db973cfce95c
Shirokuma@胡瓜 <shirokuma89.git@icloud.com>
2021-03-24 16:40:45 +0900

    main.cppをシンプルに

--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,10 @@
 #include "include/IO-Kit.h"
 
 Output LED1 = 13;
-Output LED2 = 10;
 
-Input SW1(PC13, OPEN);
-Input AnalogIN1(A1, OPEN);
-
-void setup() { Serial.begin(9600); }
+void setup() {}
 
 void loop() {
-  LED1 = LED2 = !LED1;
-  Serial.println(LED1.raw());
+  LED1 = !LED1;
   delay(300);
 }
